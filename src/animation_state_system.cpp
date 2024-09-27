616c5c2c13163b818582c7aac0831ae996ee7ee8
Sam Redway <sam.redway@outlook.com>
2024-09-27 22:39:24 +0100

    tweak animation rules

--- src/animation_state_system.cpp
+++ src/animation_state_system.cpp
@@ -14,7 +14,7 @@ void AnimationStateSystem::update() {
   // to have a field `type` for the different types of characters and
   // then have a different set of rules for each type of character
   MovementComponent& movement = movements_.at("player");
-  if (movement.velocity_x != 0) {
+  if (movement.velocity_x != 0 && movement.is_grounded) {
     animations_.at("player").current_state = AnimationState::kRunning;
   } else {
     animations_.at("player").current_state = AnimationState::kIdle;
